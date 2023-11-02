df85b3e3861d60e675fbf24f3ab9f23df5bf66dc
thitipongr <thitipong.r.psn@gmail.com>
2023-11-02 20:44:53 +0700

    try to use mui card and grid

---
+++ src/components/Overview.tsx
@@ -0,0 +1,15 @@
+import { Card, Grid } from '@mui/material'
+import React from 'react'
+
+type Props = {}
+
+const Overview = (props: Props) => {
+  return (
+    <div>Overview
+      <Card>heeehe</Card>
+      <Grid>heeehe</Grid>
+    </div>
+  )
+}
+
+export default Overview
